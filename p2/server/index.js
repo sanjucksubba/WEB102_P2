18159c8307585e38cf47c5b0d70bb8e6adcf609e
sanjucksubba <sunjucksubba@gmail.com>
2026-05-20 02:00:03 +0530

    add p2

---
+++ p2/server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Server is running!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
